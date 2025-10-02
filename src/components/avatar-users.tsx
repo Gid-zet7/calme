3cad1c77d5326f7a77c3cc0ff7c69c4d1a924df1
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2025-10-02 15:21:33 +0000

    avatar updates

--- src/components/avatar-users.tsx
+++ src/components/avatar-users.tsx
@@ -5,19 +5,19 @@ export default function AvatarUsers() {
     <div className="flex items-center rounded-full p-0.5 gap-1.5 border border-border shadow-sm shadow-black/5">
       <div className="flex -space-x-1">
         <Avatar className="size-7">
-          <AvatarImage src="/media/avatars/2.png" alt="@reui" className="border-2 border-background hover:z-10" />
+          <AvatarImage src="https://images.pexels.com/photos/5490276/pexels-photo-5490276.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="@reui" className="border-2 border-background hover:z-10" />
           <AvatarFallback>CH</AvatarFallback>
         </Avatar>
         <Avatar className="size-7">
-          <AvatarImage src="/media/avatars/4.png" alt="@reui" className="border-2 border-background hover:z-10" />
+          <AvatarImage src="https://images.pexels.com/photos/5327585/pexels-photo-5327585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="@reui" className="border-2 border-background hover:z-10" />
           <AvatarFallback>CH</AvatarFallback>
         </Avatar>
         <Avatar className="size-7">
-          <AvatarImage src="/media/avatars/6.png" alt="@reui" className="border-2 border-background hover:z-10" />
+          <AvatarImage src="https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="@reui" className="border-2 border-background hover:z-10" />
           <AvatarFallback>CH</AvatarFallback>
         </Avatar>
         <Avatar className="size-7">
-          <AvatarImage src="/media/avatars/8.png" alt="@reui" className="border-2 border-background hover:z-10" />
+          <AvatarImage src="https://images.pexels.com/photos/6156393/pexels-photo-6156393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="@reui" className="border-2 border-background hover:z-10" />
           <AvatarFallback>CH</AvatarFallback>
         </Avatar>
       </div>
